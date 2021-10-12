ad59a81ded8b8d8e85429f705795b41aa23e8ba7
Benjamin <work@benjaminrussell.me>
2021-10-12 16:29:13 -0500

    build: got scss working

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+use rocket::fs::{FileServer, relative};
 use std::collections::HashMap;
 use rocket_dyn_templates::Template;
+
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
         .mount("/", routes![index])
+        .mount("/css", FileServer::from(relative!("assets/css")))
 }
 
 #[get("/")]
 fn index() -> Template {
-        let context: HashMap<&str, &str> = [("name", "Jonathan")]
+        let context: HashMap<&str, &str> = [
+            ("name", "Benjamin"),
+            ("other", "tada")
+        ]
         .iter().cloned().collect();
         Template::render("index", context)
 }
